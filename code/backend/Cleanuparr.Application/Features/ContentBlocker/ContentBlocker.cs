0141ccfc4e8dc8297d205cd8708a2881511eaa49
Flaminel <flm.marius@gmail.com>
2025-06-30 13:54:09 +0300

    added more logs

--- code/backend/Cleanuparr.Application/Features/ContentBlocker/ContentBlocker.cs
+++ code/backend/Cleanuparr.Application/Features/ContentBlocker/ContentBlocker.cs
@@ -178,6 +178,10 @@ public sealed class ContentBlocker : GenericHandler
                             _logger.LogWarning("Download not found in any torrent client | {title}", record.Title);
                         }
                     }
+                    else
+                    {
+                        _logger.LogDebug("No torrent clients enabled");
+                    }
                 }
 
                 if (!result.ShouldRemove)
