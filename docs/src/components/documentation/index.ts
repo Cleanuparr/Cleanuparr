84dad4192f70d5182d2e1f1a4cb544a469552b2b
Flaminel <flm.marius@gmail.com>
2025-10-20 15:10:03 +0300

    renamed admonitions

--- docs/src/components/documentation/index.ts
+++ docs/src/components/documentation/index.ts
@@ -4,7 +4,7 @@ export { default as ConfigSection } from './ConfigSection';
 export { default as InstallationMethod } from './InstallationMethod';
 export { default as StepGuide, Step } from './StepGuide';
 export { default as QuickNav } from './QuickNav';
-export { default as EnhancedAdmonition, EnhancedNote, EnhancedImportant, EnhancedWarning } from './EnhancedAdmonition';
+export { default as EnhancedAdmonition, Note, Important, Warning, EnhancedNote, EnhancedImportant, EnhancedWarning } from './EnhancedAdmonition';
 export { default as FieldNavigator } from './FieldNavigator';
 
 // CSS classes for direct use
