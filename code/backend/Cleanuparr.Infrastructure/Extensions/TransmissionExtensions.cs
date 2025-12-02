343af4f03983f4cfda8688f7da92f3fc3c8ab82c
Flaminel <flm.marius@gmail.com>
2025-12-02 21:50:18 +0200

    improved some exception messages

--- code/backend/Cleanuparr.Infrastructure/Extensions/TransmissionExtensions.cs
+++ code/backend/Cleanuparr.Infrastructure/Extensions/TransmissionExtensions.cs
@@ -58,12 +58,12 @@ public static class TransmissionExtensions
     {
         if (string.IsNullOrEmpty(category))
         {
-            throw new ArgumentException(nameof(category));
+            throw new ArgumentException("Category cannot be null or empty", nameof(category));
         }
         
         if (string.IsNullOrEmpty(torrent.DownloadDir))
         {
-            throw new ArgumentException(nameof(torrent.DownloadDir));
+            throw new ArgumentException("DownloadDir cannot be null or empty", nameof(torrent.DownloadDir));
         }
 
         return string.Join(Path.DirectorySeparatorChar, Path.Combine(torrent.DownloadDir, category).Split(['\\', '/']));
