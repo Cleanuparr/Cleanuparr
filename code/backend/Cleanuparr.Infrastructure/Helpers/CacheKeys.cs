660dce0fa737a2f142288c63b29df42c91271ddb
Flaminel <flm.marius@gmail.com>
2026-02-22 19:37:03 +0200

    Fix strikes not being reset when disabling dry run (#456)

--- code/backend/Cleanuparr.Infrastructure/Helpers/CacheKeys.cs
+++ code/backend/Cleanuparr.Infrastructure/Helpers/CacheKeys.cs
@@ -4,16 +4,12 @@ namespace Cleanuparr.Infrastructure.Helpers;
 
 public static class CacheKeys
 {
-    public static string Strike(StrikeType strikeType, string hash) => $"{strikeType.ToString()}_{hash}";
-
     public static string BlocklistType(InstanceType instanceType) => $"{instanceType.ToString()}_type";
     public static string BlocklistPatterns(InstanceType instanceType) => $"{instanceType.ToString()}_patterns";
     public static string BlocklistRegexes(InstanceType instanceType) => $"{instanceType.ToString()}_regexes";
-    
+
     public static string KnownMalwarePatterns() => "KNOWN_MALWARE_PATTERNS";
-    
-    public static string StrikeItem(string hash, StrikeType strikeType) => $"item_{hash}_{strikeType.ToString()}";
-    
+
     public static string IgnoredDownloads(string name) => $"{name}_ignored";
     
     public static string DownloadMarkedForRemoval(string hash, Uri url) => $"remove_{hash.ToLowerInvariant()}_{url}";
