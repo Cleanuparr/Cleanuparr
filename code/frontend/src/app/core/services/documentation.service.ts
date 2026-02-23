64fe86c316993462ec4b889c3e0d11fa8dd0bd20
Flaminel <flm.marius@gmail.com>
2026-02-23 11:39:34 +0200

    added documentation for local auth bypass

--- code/frontend/src/app/core/services/documentation.service.ts
+++ code/frontend/src/app/core/services/documentation.service.ts
@@ -59,6 +59,9 @@ export class DocumentationService {
       'log.archiveTimeLimitHours': 'archive-time-limit',
       'ignoredDownloads': 'ignored-downloads',
       'strikeInactivityWindowHours': 'strike-inactivity-window',
+      'auth.disableLocalAuth': 'disable-auth-for-local-addresses',
+      'auth.trustForwardedHeaders': 'trust-forwarded-headers',
+      'auth.trustedNetworks': 'additional-trusted-networks',
     },
     'download-cleaner': {
       'enabled': 'enable-download-cleaner',
