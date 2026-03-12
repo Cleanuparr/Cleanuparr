74c5295223baf8bf48e24ccdd3c826bfae82c1f7
Flaminel <flm.marius@gmail.com>
2026-03-12 11:01:36 +0200

    added db migration

---
+++ code/backend/Cleanuparr.Persistence/Migrations/Users/20260312075637_AddOidcExclusiveMode.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Cleanuparr.Persistence.Migrations.Users
+{
+    /// <inheritdoc />
+    public partial class AddOidcExclusiveMode : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "oidc_exclusive_mode",
+                table: "users",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "oidc_exclusive_mode",
+                table: "users");
+        }
+    }
+}
