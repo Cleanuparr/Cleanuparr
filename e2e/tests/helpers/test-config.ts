85de80a46300c3dd817e03fe6b463dc12daa7f21
Flaminel <flm.marius@gmail.com>
2026-04-27 12:49:33 +0300

    Fix forwarded header parsing (#586)

--- e2e/tests/helpers/test-config.ts
+++ e2e/tests/helpers/test-config.ts
@@ -1,5 +1,6 @@
 export const TEST_CONFIG = {
   appUrl: 'http://localhost:5000',
+  proxyUrl: 'http://localhost:8000',
   keycloakUrl: 'http://localhost:8080',
   realm: 'cleanuparr-test',
   clientId: 'cleanuparr',
