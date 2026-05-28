a4adf23cfbc947061d9ecee6d7c75542b48718b7
Flaminel <flm.marius@gmail.com>
2026-05-28 13:21:02 +0300

    fixed doc comment

--- code/backend/Cleanuparr.Persistence/Models/Configuration/DownloadCleaner/ISeedersFilterable.cs
+++ code/backend/Cleanuparr.Persistence/Models/Configuration/DownloadCleaner/ISeedersFilterable.cs
@@ -3,7 +3,7 @@ namespace Cleanuparr.Persistence.Models.Configuration.DownloadCleaner;
 public interface ISeedersFilterable
 {
     /// <summary>
-    /// Minimum number of seeders required before cleanup. Negative values disable the check.
+    /// Minimum number of seeders required before cleanup. Set to 0 to disable.
     /// </summary>
     int MinSeeders { get; set; }
 }
