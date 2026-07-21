68dc265823b241abb0e5c30b4cf99446af3986c3
Flaminel <flm.marius@gmail.com>
2026-07-21 13:20:55 +0300

    added migration for slow rule alternate speed column

---
+++ code/backend/Cleanuparr.Persistence.Postgres/Migrations/Data/20260721100807_AddSlowRuleIgnoreAltSpeed.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Cleanuparr.Persistence.Postgres.Migrations.Data
+{
+    /// <inheritdoc />
+    public partial class AddSlowRuleIgnoreAltSpeed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "ignore_while_alt_speed_active",
+                schema: "data",
+                table: "slow_rules",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ignore_while_alt_speed_active",
+                schema: "data",
+                table: "slow_rules");
+        }
+    }
+}
---
+++ code/backend/Cleanuparr.Persistence.Sqlite/Migrations/Data/20260721100758_AddSlowRuleIgnoreAltSpeed.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Cleanuparr.Persistence.Migrations.Data
+{
+    /// <inheritdoc />
+    public partial class AddSlowRuleIgnoreAltSpeed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "ignore_while_alt_speed_active",
+                table: "slow_rules",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ignore_while_alt_speed_active",
+                table: "slow_rules");
+        }
+    }
+}
