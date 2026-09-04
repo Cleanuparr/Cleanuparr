67e3df13d3b60c27b051279b6e21be1c6c3fff76
Flaminel <flm.marius@gmail.com>
2026-09-04 14:27:38 +0300

    removed the sunset v1 stats endpoint

--- code/backend/Cleanuparr.Infrastructure/Stats/JobStats.cs
+++
@@ -1,8 +0,0 @@
-namespace Cleanuparr.Infrastructure.Stats;
-
-public class JobStats
-{
-    public Dictionary<string, JobTypeStats> ByType { get; set; } = new();
-
-    public int TimeframeHours { get; set; }
-}
